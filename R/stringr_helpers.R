e75c9f537730d0bd70d6b7e12099214d3c61f460
bansell <ansell.b@wehi.edu.au>
2024-02-08 15:43:59 +1100

    stringr helpers

--- R/stringr_helpers.R
+++ R/stringr_helpers.R
@@ -2,7 +2,7 @@
 #'
 #' Identifies sub-string occuring before or after character.
 #' Note: these are technically all_after_last() and all_before_first() occurrences.
-#' see \href{https://github.com/bansell/tidyExt#tidyext-package}{\strong{tidyExt vignette}}
+
 #'
 #' @return A vector.
 #' @param character A character string.
@@ -10,22 +10,16 @@
 #' @export
 all_after <-  function(character){glue::glue('[^{character}]*$')}
 
-
-#' @describeIn all_after Extract all characters before match.
-#'
-#' @export
-all_before <- function(character){glue::glue('[^{character}]+')}
-#'
 #' @examples
-#' requireNamespace(stringr)
-#' stringr::str_extract('string_test',all_after("_"))
-#' stringr::str_remove('string_test',all_after("_"))
+
+#' stringr::str_extract('string_test', all_after("_"))
+#' stringr::str_remove('string_test',  all_after("_"))
 #'
-#' stringr::str_extract('string/test',all_after("/"))
-#' stringr::str_extract('string/test',all_before("/"))
+#' stringr::str_extract('string/test', all_after("/"))
+#' stringr::str_extract('string/test', all_before("/"))
 #
-#' stringr::str_extract('string_test',all_before("_"))
-#' stringr::str_remove('string_test',all_before("_"))
+#' stringr::str_extract('string_test', all_before("_"))
+#' stringr::str_remove('string_test',  all_before("_"))
 #
 #
 #' stringr::str_extract('string_test_two', all_after("_"))
@@ -33,6 +27,13 @@ all_before <- function(character){glue::glue('[^{character}]+')}
 #
 #' stringr::str_extract('string_test_two', all_before("_"))
 #' stringr::str_remove('string_test_two',  all_before("_"))
+
+
+#' @describeIn all_after Extract all characters before match.
+#'
+#' @export
+all_before <- function(character){glue::glue('[^{character}]+')}
+#'
 
 
 
